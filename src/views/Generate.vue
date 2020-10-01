bbe9d6c65334c01b3514171958317add0ec43500
dafaraisya <63516652+dafaraisya@users.noreply.github.com>
2020-10-02 00:29:37 +0700

    Coba Generate dan Scan Qr Code

---
+++ src/views/Generate.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="generate">
+      <h1>Generate qr code</h1>
+      <input type="text" v-model="nrp">
+      <h1>{{ nrp }}</h1>
+      <br>
+      <vue-qr :text="nrp" :callback="test" qid="testid"></vue-qr>
+  </div>
+</template>
+
+<script>
+import VueQr from 'vue-qr'
+
+export default {
+  name: 'Generate',
+  components: {
+    VueQr
+  },
+  data() {
+      return {
+        nrp : ''
+      }
+  },
+  methods:{
+        test(dataUrl,id){
+            console.log(dataUrl, id)
+        }
+    }
+}
+</script>
---
+++ src/views/Scan.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="scan">
+      <h1>Scan qr code</h1>
+      <h2>{{ nrp }}</h2>
+      <div style=
+      "display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;">
+        <qrcode-stream @decode="onDecode"></qrcode-stream>
+      </div>
+  </div>
+</template>
+
+<script>
+import { QrcodeStream } from 'vue-qrcode-reader'
+
+export default {
+  name: 'Scan',
+  components: {
+    QrcodeStream
+  },
+  data() {
+      return {
+        nrp : ''
+      }
+  },
+  methods:{
+        onDecode (decodedString) {
+            this.nrp = decodedString
+        }
+    }
+}
+</script>
